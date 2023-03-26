fc946bdb3b9b969e6c7a805770f3a13f9551c097
Akash Samant <asamant@princeton.edu>
2023-03-26 14:53:11 -0700

    change

--- application.py
+++ application.py
@@ -2,9 +2,13 @@ import pinecone
 from flask import Flask, Response, request
 from twilio.twiml.messaging_response import MessagingResponse
 
-from constants import (DEFAULT_PERSONA, SUMMARY_TABLE_MAIN_SUMMARY,
-                       SUMMARY_TABLE_SYNTHESIS, USER_TABLE_PERSONA,
-                       USER_TABLE_PINECONE_INDEX)
+from constants import (
+    DEFAULT_PERSONA,
+    SUMMARY_TABLE_MAIN_SUMMARY,
+    SUMMARY_TABLE_SYNTHESIS,
+    USER_TABLE_PERSONA,
+    USER_TABLE_PINECONE_INDEX,
+)
 from db.api import create_new_user, fetch_link_info, fetch_user_info
 from db.embeddings import create_new_user_index
 from helpers import format_summaries_for_text, process_new_link
